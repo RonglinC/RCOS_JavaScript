4ecff899aaa8d71bde9ad6176f5ec7691e4932ae
RC <91489366+RonglinC@users.noreply.github.com>
2023-02-03 18:41:55 -0500

    Add files via upload

---
+++ Kelvin_Weather.js
@@ -0,0 +1,10 @@
+// create variable 
+var kelvin = 293;
+// convert to celsius
+var celsius = kelvin -273;
+// calculate 
+let fahrenheit;
+// round it 
+fahrenheit = Math.floor(celsius * (9/5) +32);
+
+console.log(`The temperature is ${fahrenheit}degrees Fahrenheit.`);
