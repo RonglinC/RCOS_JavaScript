0778b758b0f03207a1d7cd7a6d6bd80793cbdfb0
RC <91489366+RonglinC@users.noreply.github.com>
2023-02-03 18:41:44 -0500

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,30 @@
+// get the length of the sentence
+console.log('Hello'.length)
+
+// method
+//this start with letter 'H' so it is true
+console.log('Hey'.startsWith('H'))// print true
+
+// trim method 
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// Expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// Expected output: "Hello world!";
+
+// rndom number generator 
+// generate random number between 0 and 50
+Math.random()*50;
+
+/* the method Math.ceil return the Returns the smallest integer greater than or equal to x.
+*/
+console.log(Math.ceil(43.8))
+
+// to check if a  nubmer is an integer 
+
+/*
+objects have methods which perform actions
+we can access the properties and methods by using . dot operator
+*/
